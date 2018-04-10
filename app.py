3632fb9322ac304799e710847c762a23f8b47292
pansyjia <siyujia@umich.edu>
2018-04-10 15:29:06 -0400

    fball page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import model
 
 app = Flask(__name__)
@@ -13,9 +13,47 @@ def index():
         </ul>
     '''
 
-@app.route('/bball')
+@app.route('/bball', methods=['GET', 'POST'])
 def bball():
-    return render_template("seasons.html", seasons=model.get_bball_seasons())
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_bball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_bball_seasons()
+
+    return render_template("seasons.html", seasons=seasons)
+
+
+@app.route('/fball', methods=['GET', 'POST'])
+def fball():
+    if request.method == 'POST':
+        sortby = request.form['sortby']
+        sortorder = request.form['sortorder']
+        seasons = model.get_fball_seasons(sortby, sortorder)
+    else:
+        seasons = model.get_fball_seasons()
+
+    return render_template("seasons2.html", seasons=seasons)
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    if request.method == 'POST':
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+    else:
+        firstname = ''
+        lastname = ''
+
+    return render_template("hello.html", firstname=firstname, lastname=lastname)
+    # equals the local variable firstname
+
 
 if __name__ == '__main__':
+    model.init_bball()
+    model.init_fball()
+    # print(model.get_bball_seasons())
+    model.get_bball_seasons()
+    model.get_fball_seasons()
     app.run(debug=True)
